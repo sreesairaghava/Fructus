f94b1ff46db51bab9d460636ed75df6a9dafed0e
Sree Sai Raghava <raghava.rym@gmail.com>
2020-11-22 17:40:26 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sree Sai Raghava Dandu on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
